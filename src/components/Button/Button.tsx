b84e00c4361090f70da1874c464bff70d30f6fff
Karel <6916288+karelkangro@users.noreply.github.com>
2024-05-21 09:47:45 +0300

    adjust button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 
-export const Button = styled.button<{ primary?: boolean; }>`
+export const Button = styled.button<{ $primary?: boolean; }>`
   display: flex;
   gap: var(--space-1);
   justify-content: center;
   align-items: center;
-  padding: var(--space-05) var(--space-2);
-  color: ${props => props.primary ? 'var(--color-secondary)' : 'var(--color-primary)'};
-  border-radius: var(--radius-xs);
-  border: var(--border);
-  background: ${props => props.primary ? 'var(--color-primary)' : 'none'};
+  padding: var(--space-15) var(--space-25);
+  color: ${props => props.$primary ? 'var(--color-text)' : 'var(--color-primary)'};
+  font-size: var(--font-size-xs);
+  font-weight: var(--font-weight-regular);
+  line-height: var(--line-height-xs);
+  border: none;
+  border-radius: var(--radius-2xs);
+  background: ${props => props.$primary ? 'var(--color-primary)' : 'none'};
 `;
