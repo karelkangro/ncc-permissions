eb8203d86d9d175e29bfeef380f832b264ff5eb7
Karel <6916288+karelkangro@users.noreply.github.com>
2024-05-21 09:46:22 +0300

    add icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,29 @@
-import ViteLogo from "./vite.svg?react";
-import IconVerticalDots from "./dots-vertical.svg?react";
+import UserIcon from "./user.svg?react";
+import FlagIcon from "./flag.svg?react";
+import LockIcon from "./lock.svg?react";
+import CloseIcon from "./close-icon.svg?react";
+import UsersIcon from "./users.svg?react";
+import PencilIcon from "./pencil.svg?react";
+import GarbageIcon from "./garbage.svg?react";
+import IndicatorIcon from "./indicator.svg?react";
+import BulletListIcon from "./bullet-list.svg?react";
+import ChevronDownIcon from "./chevron-down.svg?react";
+import ChevronLeftIcon from "./chevron-left.svg?react";
+import DocumentListIcon from "./document-list.svg?react";
+import DotsVerticalIcon from "./dots-vertical.svg?react";
 
 export {
-  ViteLogo,
-  IconVerticalDots,
-}
+  UserIcon,
+  FlagIcon,
+  LockIcon,
+  CloseIcon,
+  UsersIcon,
+  PencilIcon,
+  GarbageIcon,
+  IndicatorIcon,
+  BulletListIcon,
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  DocumentListIcon,
+  DotsVerticalIcon,
+};
