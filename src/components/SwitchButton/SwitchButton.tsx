8e4ecf5297a6c4bc2e191dadbcaadd40929a1dd8
Karel <6916288+karelkangro@users.noreply.github.com>
2024-05-28 08:17:53 +0300

    add ref for tooltip to work

--- src/components/SwitchButton/SwitchButton.tsx
+++ src/components/SwitchButton/SwitchButton.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react';
+import { useState, forwardRef } from 'react';
 import { Container, Label, Input, Switch } from './SwitchButton.styled';
 
 interface ISwitchButton {
   disabled?: boolean;
   checked: boolean;
-  onChange: (checked: boolean) => void;
+  onChange: (checked?: boolean) => void;
 }
 
-export const SwitchButton = ({ disabled, checked, onChange }: ISwitchButton) => {
-  const [isOn, setIsOn] = useState(checked);
+export const SwitchButton = forwardRef<HTMLInputElement, ISwitchButton>(
+  ({ disabled, checked, onChange }, ref) => {
+    const [isOn, setIsOn] = useState(checked);
 
-  const handleSwitch = () => {
-    if (!disabled) {
-      const newIsOn = !isOn;
-      setIsOn(newIsOn);
-      onChange(newIsOn);
-    }
-  };
+    const handleSwitch = () => {
+      if (!disabled) {
+        const newIsOn = !isOn;
+        setIsOn(newIsOn);
+        onChange(newIsOn);
+      }
+    };
 
-  return (
-    <Container>
-      <Label $disabled={disabled} $isOn={isOn}>
-        <Input
-          type="checkbox"
-          checked={isOn}
-          onChange={handleSwitch}
-          aria-checked={isOn}
-        />
-        <Switch $disabled={disabled} $isOn={isOn} />
-      </Label>
-    </Container>
-  );
-};
+    return (
+      <Container>
+        <Label $disabled={disabled} $isOn={isOn}>
+          <Input
+            ref={ref}
+            type="checkbox"
+            checked={isOn}
+            onChange={handleSwitch}
+            aria-checked={isOn}
+          />
+          <Switch $disabled={disabled} $isOn={isOn} />
+        </Label>
+      </Container>
+    );
+  }
+);
